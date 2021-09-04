73728628a0f34d26fd6cf4436840e1d92db1a8af
Denise Giubilei <giubileidenise@gmail.com>
2021-09-04 16:29:19 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import { createConnection } from "typeorm";
 import { routes } from "./routes";
 
@@ -7,6 +9,8 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
